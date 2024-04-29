64edcaa0ce3ee8f5cb95b1a6731b6145db799dc5
cartudev <cartcarll@gmail.com>
2024-04-29 02:11:40 -0300

    added carousel, forms, cleaning pages, installed plugin for url shortcuts, created login forms, edited custom js

--- wp-content/themes/ninatheme/assets/js/custom.js
+++ wp-content/themes/ninatheme/assets/js/custom.js
@@ -1,12 +1,29 @@
-console.log('Hola WordPress');
+
+let search = document.querySelector('.search-field');
+console.log(search)
+search.placeholder='Buscar';
 
 
+console.log(tns)
 
-
-
-    let search = document.querySelector('.search-field');
-    console.log(search)
-    search.placeholder='Buscar';
+  var slider = tns({
+    "container": ".slider",
+    "items": 3.5,
+    "fixedWidth": 455,
+    //"autoWidth": true,
+    "speed": 400,
+    "slideBy": "1",
+    "mouseDrag": true,
+    "swipeAngle": false,
+    "speed": 400,
+    "autoplay": true,
+    "autoplayHoverPause": true,
+    "autoplayButtonOutput": false,
+    "controls": false,
+    "nav": false,
+//    "edgePadding": 50,
+//    "gutter": 50,
+  });
 
 
 
